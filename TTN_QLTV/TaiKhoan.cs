9a6a186e0c7bbd445a827b1019a96ad6fef9bce4
Tranthu-CNTT <61007321+Tranthu-CNTT@users.noreply.github.com>
2020-06-23 23:53:29 +0700

    Add files via upload

---
+++ TTN_QLTV/TaiKhoan.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TTN_QLTV
+{
+    public partial class TaiKhoan : Form
+    {
+        public TaiKhoan()
+        {
+            InitializeComponent();
+        }
+
+        private void TaiKhoan_Load(object sender, EventArgs e)
+        {
+           
+        }
+        private bool validate()
+        {
+            
+        }
+
+        private void hien()
+        {
+            
+        }
+
+        private void binding()
+        {
+            
+
+        }
+
+        private void btThem_Click(object sender, EventArgs e)
+        {
+            
+                
+            
+        }
+
+        private void btSua_Click(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void btXoa_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btthoat_Click(object sender, EventArgs e)
+        {
+
+            
+        }
+    }
+}
